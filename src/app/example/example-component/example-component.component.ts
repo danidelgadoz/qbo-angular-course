e1328bdaa51bec2b4b182e334b94b09b1b9ba255
Dandel <dedd1993@gmail.com>
2021-10-06 21:10:14 -0500

    undestanding angular interpolation

--- src/app/example/example-component/example-component.component.ts
+++ src/app/example/example-component/example-component.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./example-component.component.scss']
 })
 export class ExampleComponentComponent implements OnInit {
+  course = {
+    name: 'Angular for Developers',
+    price: 100
+  };
 
   constructor() { }
 
