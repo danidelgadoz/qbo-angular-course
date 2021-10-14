34c7496554c91600b2e532d9451137a87e3b4808
Dandel <dedd1993@gmail.com>
2021-10-13 20:13:27 -0500

    add examples of angular material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterialModule } from './shared/material.module';
 import { AppComponent } from './app.component';
 import { ExampleComponentComponent } from './example/example-component/example-component.component';
 import { ExampleBootstrapComponent } from './example/example-bootstrap/example-bootstrap.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExampleAngularMaterialComponent } from './example/example-angular-material/example-angular-material.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleComponentComponent,
-    ExampleBootstrapComponent
+    ExampleBootstrapComponent,
+    ExampleAngularMaterialComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
