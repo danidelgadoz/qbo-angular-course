baef24c91bae6fb474c97f1cfe36eaeae0bdfcc8
Dandel <dedd1993@gmail.com>
2021-10-22 21:47:01 -0500

    feat(login): integrate with oauth api

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
 
   constructor(
+    private authService: AuthService,
     private router: Router,
   ) { }
 
@@ -23,9 +25,17 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log('onSubmit', this.loginForm.value);
-    // sessionStorage.setItem('userSession', '3661411c65331184ac73d8660d0b4648');
-    // this.router.navigate(['dashboard']);
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password)
+      .subscribe(
+        (data) => {
+          sessionStorage.setItem('userSession', '3661411c65331184ac73d8660d0b4648');
+          this.router.navigate(['dashboard']);
+        },
+      );
+
+
   }
 
 }
