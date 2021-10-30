e19ea8634f7da879fac1305b1172a57a30cdec59
Dandel <dedd1993@gmail.com>
2021-10-29 21:52:36 -0500

    refactor(shared): create shared module to reuse commons

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from '../shared/shared.module';
 
-import { MaterialModule } from '../shared/material.module';
 import { AuthRoutingModule } from './auth-routing.module';
-
 
 @NgModule({
   declarations: [
     ...AuthRoutingModule.components,
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
-    MaterialModule,
+    SharedModule,
     AuthRoutingModule,
   ]
 })
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { MaterialModule } from '../shared/material.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HighlightDirective } from './example/example-directives-and-pipes/highlight.directive';
 import { OnlyNumber } from './example/example-directives-and-pipes/only-number.directive';
 import { ReversePipe } from './example/example-directives-and-pipes/reverse.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,11 +14,7 @@ import { ReversePipe } from './example/example-directives-and-pipes/reverse.pipe
     ...DashboardRoutingModule.components
   ],
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    FlexLayoutModule,
-    MaterialModule,
+    SharedModule,
     DashboardRoutingModule
   ]
 })
